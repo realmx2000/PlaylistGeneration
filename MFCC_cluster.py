721e3e470aa9ea39d935b1c02a18cd5e5dc95873
ejones313 <erik.jones313@gmail.com>
2017-11-28 23:02:33 -0800

    Add files via upload

---
+++ MFCC_cluster.py
@@ -0,0 +1,172 @@
+import numpy as np
+from sklearn.cluster import KMeans
+from scipy.stats import multivariate_normal
+
+def reconstruct(tm_flat):
+	return(np.reshape(tm_flat, (-1, int(tm_flat.shape[0]/12))))
+
+def cluster_song(tm, threshhold = 10, min_clusters = 4, max_it = 100):
+	# TUNE THESE
+	#.0001 increases the spread of numbers of clusters, but not the mean number.
+	threshold = 10000
+	#Lots of songs naturally go to 1 cluster essentailly regardless of threshhold without this.
+	min_clusters = 1
+
+	kmeans = None
+	k = 30
+	first = True
+
+	converged = False
+	it = 0
+	distortion = 0
+	while (k > min_clusters and it < max_it):
+		it += 1
+		if (first):
+			first = False
+			kmeans = KMeans(n_clusters=k, max_iter=1).fit(tm)
+			centroids = kmeans.cluster_centers_
+			continue; 
+
+		kmeans = KMeans(n_clusters=k, init=centroids, max_iter=1).fit(tm)
+		new_centroids = collapse_centroids(kmeans)
+		new_distortion = 0
+		for i in range(kmeans.labels_.shape[0]):
+			centroid_assignment = centroids[kmeans.labels_[i]]
+			error = tm[i,:] - centroid_assignment
+			#print(error.shape)
+			new_distortion += np.dot(error.T, error)
+		#print(distortion)
+		if(new_centroids.shape == centroids.shape):
+			print (new_distortion - distortion)
+
+
+		if (new_centroids.shape == centroids.shape and np.linalg.norm(new_distortion - distortion) < threshold):
+			#print("TRUE")
+			centroids = new_centroids
+			converged = True
+			break
+		else:
+			k = len(new_centroids)
+			centroids = new_centroids
+		distortion = new_distortion
+	if it == max_it:
+		print("Did this")
+		kmeans = KMeans(n_clusters=k).fit(tm)
+		converged = True
+
+
+	if not converged:
+		kmeans = KMeans(n_clusters=k, init=centroids).fit(tm)
+		centroids = kmeans.cluster_centers_
+
+	return centroids, kmeans.labels_
+
+def generate_cloud(timbre_matrix, window_length):
+	length = timbre_matrix.shape[1]
+	vectors = np.zeros((length - window_length, (window_length+1) * 12))
+	for index in range(length - window_length):
+		vector = timbre_matrix[:,index:index + window_length].flatten()
+		deltas = timbre_matrix[:,index+window_length] - timbre_matrix[:,index]
+		vectors[index,:] = np.concatenate((vector,deltas))
+		prev_vector = vector
+	return(vectors)
+
+
+def collapse_centroids(kmeans):
+	# TUNE THIS
+	threshold = 10
+
+	centroids = kmeans.cluster_centers_
+	labels = kmeans.labels_
+	counts = {}
+	for i in labels:
+		if i not in counts:
+			counts[i] = 0
+		counts[i] += 1
+
+	shape = centroids.shape[0]
+	distances = np.zeros((shape,shape))
+	for i in range(shape):
+		distances[i][i] = float("inf")
+		for j in range(i+1,shape):
+			distances[i][j] = np.linalg.norm(centroids[j]-centroids[i])
+			distances[j][i] = distances[i][j]
+
+	new_centroids = []
+	already_combined = set()
+	for i in range(shape):
+		min_dist = min(distances[i]);
+		min_index = np.nonzero(distances[i] == min_dist)[0][0]
+		if ((min_dist < threshold or counts[i] < 73) and i not in already_combined):
+			new_centroid =(counts[i]*centroids[i] + counts[min_index]*centroids[min_index])/(counts[i]+counts[min_index])
+			new_centroids.append(new_centroid)
+			already_combined.add(min_index)
+			for j in range(shape):
+				distances[j,i] = float('inf')
+				distances[min_index,j] = float('inf')
+
+		elif (i not in already_combined):
+			new_centroids.append(centroids[i])
+
+	return(np.array(new_centroids))
+
+
+#M-step, but just with the priors.
+def maximize_priors(cloud, means, covariances):
+	priors = np.zeros((1, means.shape[1]))
+	priors.fill(1/means.shape[1])
+	old_priors = np.zeros((1, means.shape[0]))
+	while(np.linalg.norm(priors-old_priors) > 0.1):
+		posteriors = calculate_posteriors(cloud, means, covariances, priors)
+		old_priors = priors
+		priors = np.sum(posteriors, axis=0, keepdims=True)
+		priors /= cloud.shape[0]
+	return priors
+
+#E-step: Calculate soft guesses for latent variables.
+def calculate_posteriors(data, means, covariances, priors):
+	posteriors = np.zeros((data.shape[0],means.shape[1]))
+	for i in range(data.shape[0]):
+		marginal = 0
+		x = data[i,:]
+		for j in range(means.shape[1]):
+			mean = means[:,j]
+			covariance = covariances[j,:,:]
+			prior = priors[0,j]
+			likelihood = multivariate_normal.pdf(x, mean, covariance)
+			posterior = likelihood * prior
+			marginal += posterior
+			posteriors[i,j] = posterior
+		posteriors[i, :] /= marginal
+
+	return posteriors
+
+#Means and covariances can be calculated with np.mean and np.cov, respectively.
+
+#Calculates the fast spectral distance between two GMM models by using the 
+#cross similarity metric, normalized by the self similarity, as described in one of
+#the papers.
+def calculate_distance(priors1, priors2, means1, means2, covariances1, covariances2):
+	L_AA = likelihood(priors1, priors1, means1, means1, covariances1)
+	L_BB = likelihood(priors2, priors2, means2, means2, covariances2)
+	self_similarity = L_AA + L_BB
+	L_AB = likelihood(priors1, priors2, means1, means2, covariances2)
+	L_BA = likelihood(priors2, priors1, means2, means1, covariances1)
+	cross_similarity = L_AB + L_BA
+	distance = cross_similarity - self_similarity
+	return distance
+
+#Calculates the likelihood of a set of centroids (means1, priors1) occuring given 
+#the GMM defined by means2, covariances, priors2.
+def likelihood(priors1, priors2, means1, means2, covariances):
+	likelihood = 0
+	for i in range(priors1.shape[1]):
+		weight_a = priors1[0,i]
+		likelihood_a = 0
+		for j in range(priors2.shape[1]):
+			weight_b = priors2[0,j]
+			likelihood_b = multivariate_normal.pdf(means1[:,i], mean=means2[:,j], cov=covariances[j,:,:])
+			likelihood_a += weight_b * likelihood_b
+		likelihood_a = np.log(likelihood_a)
+		likelihood += weight_a * likelihood_a
+	return likelihood
---
+++ song_history_cluster.py
@@ -0,0 +1,152 @@
+import numpy as np
+from numpy import newaxis
+from sklearn.cluster import KMeans
+
+import MFCC_cluster as tm_cl
+
+
+# Accepts mfcc matrices (reconstructed, not cloud)
+def MFCC_dists(mfccs):
+    num_songs = len(mfccs)
+    dist_matrix = np.zeros((num_songs, num_songs))
+
+    centers = [None] * num_songs
+    labels = [None] * num_songs
+    cloud = [None] * num_songs
+    for i in range(num_songs):
+        cloud[i] = tm_cl.generate_cloud(mfccs[i].astype(float), 5)
+        centers[i], labels[i] = tm_cl.cluster_song(cloud[i])
+        print("Final num clusters: ", len(centers[i]))
+        cloud[i] = cloud[i].T
+        centers[i] = centers[i].T
+
+    for i in range(num_songs):
+        num_clusters_i = centers[i].shape[1]
+        covs1 = [None] * num_clusters_i
+        for l in range(num_clusters_i):
+            covs1[l] = np.cov(cloud[i][:,labels[i] == l])
+        covs1 = np.array(covs1)
+
+        for j in range(i+1, num_songs):
+            num_clusters_j = centers[j].shape[1]
+            covs2 = [None] * num_clusters_j
+            for m in range(num_clusters_j):
+                covs2[m] = np.cov(cloud[j][:,labels[j] == m])
+            covs2 = np.array(covs2)
+
+            priors1 = tm_cl.maximize_priors(cloud[i].T, centers[i], covs1)
+            priors2 = tm_cl.maximize_priors(cloud[j].T, centers[j], covs2)
+
+            dist_matrix[i,j] = tm_cl.calculate_distance(priors1, priors2, centers[i], centers[j], covs1, covs2)
+            dist_matrix[j,i] = dist_matrix[i,j]
+    return(dist_matrix)
+
+def tag_differences(tag_lists):
+    num_songs = len(tag_lists)
+    tag_matrix = np.zeros((num_songs, num_songs))
+    for i in range(num_songs):
+        for j in range(i+1, num_songs):
+            shared = len(tag_lists[i] & tag_lists[j])
+            total = len(tag_lists[i] | tag_lists[j])
+            tag_matrix[i,j] = 1-(float(shared)/total)
+            tag_matrix[j,i] = tag_matrix[i,j]
+    return(tag_matrix)
+
+# k means implementation including the MFCC distances and tags
+def k_means(data, c_count, max_iter, dist_matrix, tag_matrix):
+    data = data.astype(np.float64)
+    indices = np.random.choice(data.shape[0], c_count, replace=False)
+    # initial centers
+    mus = data[indices, :]
+    # initial cluster dists
+    cluster_mfcc_dists = dist_matrix[indices]
+    cluster_tag_dists = tag_matrix[indices]
+    distortion = 0
+    prev_distortion = 0
+    
+    for rep in range(max_iter):
+        # choose centers
+        diff = data[newaxis, :, :] - mus[:, newaxis, :]
+        dist = np.sqrt(np.square(np.linalg.norm(diff, axis=2)) + np.square(cluster_mfcc_dists) + np.square(cluster_tag_dists))
+        cs = np.argmin(dist, axis=0)
+
+        # compute new centers
+        for j in range(mus.shape[0]):
+            mus[j] = np.sum(data[cs == j], axis=0) / len(data[cs == j])
+
+        # calculate distance rows of new clusters
+        for j in range(c_count):
+            cluster_mfcc_dists[j] = np.sum(dist_matrix[cs == j], axis=0) / len(dist_matrix[cs == j])
+            cluster_tag_dists[j] = np.sum(tag_matrix[cs == j], axis=0) / len(tag_matrix[cs == j])
+
+        # compute distortion
+        distortion = 0
+        for i in range(data.shape[0]):
+            error = data[i,:] - mus[cs[i]]
+            distortion += error.dot(error) + np.square(cluster_mfcc_dists[cs[i],i]) + np.square(cluster_tag_dists[cs[i],i])
+
+        # break if distortion does not change
+        if rep != 0 and prev_distortion - distortion < .01:
+            break
+        prev_distortion = distortion
+
+    return cs, mus, cluster_mfcc_dists, cluster_tag_dists
+
+
+
+
+# Script
+
+# TUNE THIS
+#weight = [np.array([1,1,1,1,1,1,1,1]),np.array([1,1,1,1/200,1,1/10,1,1]),np.array([0,0,0,0,0,0,0,1]),np.array([1,1,1,1/200,1,1/10,0,1]),np.array([1,1,1,1/200,1,1/10,1,.01]),np.array([0,0,0,1/200,1,1/10,1,1]),np.array([0,0,0,1/200,1,1/10,1,1/100]),np.array([1/10,1/10,1/10,1/200,1,1/10,1,2]),np.array([1,1,1,1,1,1,1,5]),np.array([1,0,1,.01,.4,.3,.3,.1])]
+
+
+"""
+[0 0 0 0 0 0]
+[1 1 1 0 1 1]
+[0 0 1 0 0 0]
+[1 1 1 1 0 1]
+[0 0 0 1 0 0]
+[1 1 1 0 1 1]
+[0 0 0 1 1 0]
+[0 0 0 0 1 0]
+[1 1 1 1 0 1]
+[1 1 0 1 1 1]"""
+
+#SONGS
+"""
+("I Didn't Mean To", 'Casual') Rap
+('Soul Deep', 'The Box Tops') Folk
+('Amor De Cabaret', 'Sonora Santanera') Weird slow Spanish
+('Something Girls', 'Adam Ant') Folk/Rock
+('Face the Ashes', 'Gob') Rock/Metal
+('The Moon And I (Ordinary Day Album Version)', 'Jeff And Sheri Easter') Country
+"""
+weight = [np.array([1,1,1,1,1,1,1,1]),np.array([1,1,1,1/200,1,1/10,1,1]),np.array([0,0,0,0,0,0,0,1]),np.array([1,1,1,1/200,1,1/10,0,1]),np.array([1,1,1,1/200,1,1/10,1,.01]),np.array([0,0,0,1/200,1,1/10,1,1]),np.array([0,0,0,1/200,1,1/10,1,1/100]),np.array([1/10,1/10,1/10,1/200,1,1/10,1,2]),np.array([1,1,1,1,1,1,1,5]),np.array([1,0,1,.01,.4,.3,.3,.1])]
+for weights in weight:
+
+    data = np.load('500Songs.npy')
+    print("Data loaded")
+
+    num_songs = data.shape[0]
+    num_songs_to_cluster = 400
+    tms = [None] * num_songs_to_cluster
+    converted = [None] * num_songs_to_cluster
+    for i in range(0, num_songs_to_cluster):
+        tms[i] = tm_cl.reconstruct(data[i][7:])
+        converted[i]= data[i][0:2].astype(np.float64)
+        converted[i] = np.concatenate((converted[i], data[i][3:7].astype(np.float64)))
+        converted[i] = np.concatenate((converted[i], [set(data[i][2].decode('UTF-8').split('\t'))]))
+    converted = np.array(converted)
+
+    tag_diffs = weights[6]*tag_differences(converted[:,6])
+    print("Data processed, tag matrix calculated")
+
+    mfcc_diffs = weights[7]*MFCC_dists(tms)
+    print("MFCC matrix calculated")
+    print(converted[:,0:6])
+    converted = np.dot(converted[:,0:6],np.diag(weights[0:6]))
+    cs, mus, cluster_mfcc_dists, cluster_tag_dists = k_means(converted[0:num_songs_to_cluster], 2, 20, mfcc_diffs, tag_diffs)
+
+    print(cs)
+
