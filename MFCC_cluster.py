17e229a5fe7c6a1277322b926d53cd50a3b9d53b
Milind Jagota <mjagota@stanford.edu>
2017-12-02 20:23:07 -0800

    It works
    Yay

--- MFCC_cluster.py
+++ MFCC_cluster.py
@@ -5,14 +5,22 @@ from scipy.stats import multivariate_normal
 def reconstruct(tm_flat):
 	return(np.reshape(tm_flat, (-1, int(tm_flat.shape[0]/12))))
 
-def cluster_song(tm):
-	threshold = 10
+def cluster_song(tm, threshhold = 10, min_clusters = 4, max_it = 100):
+	# TUNE THESE
+	#.0001 increases the spread of numbers of clusters, but not the mean number.
+	threshold = 1000
+	#Lots of songs naturally go to 1 cluster essentailly regardless of threshhold without this.
+	min_clusters = 1
+
 	kmeans = None
 	k = 30
 	first = True
 
 	converged = False
-	while (k > 4):
+	it = 0
+	distortion = 0
+	while (k > min_clusters and it < max_it):
+		it += 1
 		if (first):
 			first = False
 			kmeans = KMeans(n_clusters=k, max_iter=1).fit(tm)
@@ -21,18 +29,65 @@ def cluster_song(tm):
 
 		kmeans = KMeans(n_clusters=k, init=centroids, max_iter=1).fit(tm)
 		new_centroids = collapse_centroids(kmeans)
-		if (new_centroids.shape == centroids.shape and np.linalg.norm(new_centroids - centroids) < threshold):
+		new_distortion = 0
+		for i in range(kmeans.labels_.shape[0]):
+			centroid_assignment = centroids[kmeans.labels_[i]]
+			error = tm[i,:] - centroid_assignment
+			new_distortion += np.dot(error.T, error)
+
+		if (new_centroids.shape == centroids.shape and np.linalg.norm(new_distortion - distortion) < threshold):
 			centroids = new_centroids
 			converged = True
 			break
 		else:
 			k = len(new_centroids)
 			centroids = new_centroids
+		distortion = new_distortion
+	if it == max_it:
+		kmeans = KMeans(n_clusters=k).fit(tm)
+		converged = True
 	if not converged:
 		kmeans = KMeans(n_clusters=k, init=centroids).fit(tm)
 		centroids = kmeans.cluster_centers_
 
-	return centroids, kmeans.labels_
+	not_usable = set()
+	usable = set()
+	keep = []
+	labels = list(kmeans.labels_)
+	counts = {}
+	new_centroids = []
+	for i in labels:
+		if i not in counts:
+			counts[i] = 0
+		counts[i] += 1
+	for count in counts:
+		if counts[count] < 2:
+			not_usable.add(count)
+		else:
+			usable.add(count)
+			new_centroids.append(centroids[count])
+
+	for i in range(len(labels)):
+		if labels[i] in usable:
+			keep.append(i)
+			new_list = list(not_usable)
+			new_list.append(int(labels[i]))
+			new_list.sort()
+			ind = new_list.index(labels[i])
+			labels[i] -= ind
+	keep = np.array(keep)
+
+	return np.array(new_centroids), np.array(labels)[keep], tm[keep,:]
+
+
+def assign(original_index, usable, centroids):
+	position = centroids[original_index]
+	index = -1
+	min_distance = float('inf')
+	for i in usable:
+		if np.linalg.norm(centroids[i]-position) < min_distance:
+			index = i
+	return index
 
 def generate_cloud(timbre_matrix, window_length):
 	length = timbre_matrix.shape[1]
@@ -45,8 +100,29 @@ def generate_cloud(timbre_matrix, window_length):
 	return(vectors)
 
 
+def remove_one_clusters(kmeans):
+	centroids = kmeans.cluster_centers_
+	labels = kmeans.labels_
+	counts = {}
+	new_centroids = []
+	shape = centroids.shape[0]
+
+	for i in labels:
+		if i not in counts:
+			counts[i] = 0
+		counts[i] += 1
+
+	for i in range(shape):
+		if counts[i] > 1:
+			new_centroids.append(centroids[i])
+
+	return(new_centroids)
+
+
+
 def collapse_centroids(kmeans):
-	threshold = 250
+	# TUNE THIS
+	threshold = 200
 
 	centroids = kmeans.cluster_centers_
 	labels = kmeans.labels_
@@ -64,12 +140,13 @@ def collapse_centroids(kmeans):
 			distances[i][j] = np.linalg.norm(centroids[j]-centroids[i])
 			distances[j][i] = distances[i][j]
 
+
 	new_centroids = []
 	already_combined = set()
 	for i in range(shape):
 		min_dist = min(distances[i]);
 		min_index = np.nonzero(distances[i] == min_dist)[0][0]
-		if (min_dist < threshold and i not in already_combined):
+		if ((min_dist < threshold) and i not in already_combined):
 			new_centroid =(counts[i]*centroids[i] + counts[min_index]*centroids[min_index])/(counts[i]+counts[min_index])
 			new_centroids.append(new_centroid)
 			already_combined.add(min_index)
@@ -87,15 +164,13 @@ def collapse_centroids(kmeans):
 def maximize_priors(cloud, means, covariances):
 	priors = np.zeros((1, means.shape[1]))
 	priors.fill(1/means.shape[1])
-	old_priors = np.zeros((1, means.shape[0]))
-	index = 0
-	while(index < 2 or (np.linalg.norm(priors-old_priors) > 0.1)):
-		print('Iteration: %d' % index)
-		index += 1
+	delta = 10
+	while(delta > 0.1):
 		posteriors = calculate_posteriors(cloud, means, covariances, priors)
 		old_priors = priors
 		priors = np.sum(posteriors, axis=0, keepdims=True)
 		priors /= cloud.shape[0]
+		delta = np.linalg.norm(priors-old_priors)
 	return priors
 
 #E-step: Calculate soft guesses for latent variables.
@@ -108,25 +183,16 @@ def calculate_posteriors(data, means, covariances, priors):
 			mean = means[:,j]
 			covariance = covariances[j,:,:]
 			prior = priors[0,j]
-
 			likelihood = multivariate_normal.pdf(x, mean, covariance)
 			posterior = likelihood * prior
 			marginal += posterior
 			posteriors[i,j] = posterior
 		if marginal == 0:
-			posteriors[i, :].fill(1 / means.shape[1])
+			posteriors[i,:].fill(1 / means.shape[1])
 		else:
-			posteriors[i, :] /= marginal
+			posteriors[i,:] /= marginal
 
 	return posteriors
-
-#def calculate_log_marginals(posteriors):
-#	first = posteriors[:,0].reshape(-1, 1)
-#	posteriors -= first
-#	exponentials = np.exp(posteriors)
-#	higher_terms = np.log(np.sum(exponentials[:,1:], axis=1))
-#	log_marginals = np.log1p(higher_terms)
-#	return log_marginals
 
 #Means and covariances can be calculated with np.mean and np.cov, respectively.
 
@@ -157,15 +223,3 @@ def likelihood(priors1, priors2, means1, means2, covariances):
 		likelihood_a = np.log(likelihood_a)
 		likelihood += weight_a * likelihood_a
 	return likelihood
-
-	
-#data = np.load('500Songs.npy')
-#num_songs = data.shape[0]
-
-
-#print(data[0][0:7])
-#tm = reconstruct(data[0][7:])
-#tm = generate_cloud(tm.astype(float), 5)
-#centers, labels = cluster_song(tm)
-#print(centers.shape)
-
