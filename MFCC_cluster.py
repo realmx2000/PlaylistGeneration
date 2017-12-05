Fixed numerical instability in posteriors calculation

MFCC_cluster.py
@@ -180,22 +180,16 @@ def maximize_priors(cloud, means, covariances):
 def calculate_posteriors(data, means, covariances, priors):
 	posteriors = np.zeros((data.shape[0],means.shape[1]))
 	for i in range(data.shape[0]):
-		marginal = 0
 		x = data[i,:]
 		for j in range(means.shape[1]):
 			mean = means[:,j]
 			covariance = covariances[j,:,:]
 			prior = priors[0,j]
-			likelihood = multivariate_normal.pdf(x, mean, covariance)
-			posterior = likelihood * prior
-			marginal += posterior
+			log_like = multivariate_normal.logpdf(x, mean, covariance)
+			posterior = np.log(prior) + log_like
 			posteriors[i,j] = posterior
-
-		if marginal == 0:
-			posteriors[i,:].fill(1 / means.shape[1])
-		else:
-			posteriors[i,:] /= marginal
-	return posteriors
+		posteriors[i,:] -= sp.logsumexp(posteriors[i,:])
+	return np.exp(posteriors)
 
 #Means and covariances can be calculated with np.mean and np.cov, respectively.
 
@@ -217,10 +211,10 @@ def calculate_distance(priors1, priors2, means1, means2, covariances1, covarianc
 def likelihood(priors1, priors2, means1, means2, covariances):
 	likelihood = 0
 	for i in range(priors1.shape[1]):
-		weight_a = priors1[0,i]
+		weight_a = priors1[0,i] + 1e-100
 		likelihoods_b = np.zeros((1, priors2.shape[1]))
 		for j in range(priors2.shape[1]):
-			weight_b = priors2[0,j]
+			weight_b = priors2[0,j] + 1e-100
 			log_prob = multivariate_normal.logpdf(means1[:,i], mean=means2[:,j], cov=covariances[j,:,:])
 			likelihoods_b[0,j] = np.log(weight_b) + log_prob
 

song_history_cluster.py
@@ -20,6 +20,7 @@ def MFCC_dists(mfccs):
         centers[i] = centers[i].T
 
     for i in range(num_songs):
+        print("Distance from %d" % i)
         num_clusters_i = centers[i].shape[1]
         covs1 = [None] * num_clusters_i
         for l in range(num_clusters_i):
@@ -33,6 +34,7 @@ def MFCC_dists(mfccs):
             counts1[w] += 1
 
         for j in range(i+1, num_songs):
+            print("To %d" % j)
 
             counts2 = {}
             for w in labels[j]:
@@ -139,7 +141,9 @@ def k_means(data, c_count, max_iter, dist_matrix, tag_matrix):
 ('Face the Ashes', 'Gob') Rock/Metal
 ('The Moon And I (Ordinary Day Album Version)', 'Jeff And Sheri Easter') Country
 """
-weight = [np.array([1,1,1,1,1,1,1,1,1]),np.array([1,1,1,1,1/200,1,1/10,1,.01]),np.array([1,1,1,1,1/200,1,1/10,1,1]),np.array([0,0,0,0,0,0,0,0,1]),np.array([1,1,1,1,1/200,1,1/10,0,1]),np.array([1,0,0,0,1/200,1,1/10,1,1]),np.array([1,0,0,0,1/200,1,1/10,1,1/100]),np.array([1,1/10,1/10,1/10,1/200,1,1/10,1,2]),np.array([1,1,1,1,1,1,1,1,5]),np.array([1,1,0,1,.01,.4,.3,.3,.1])]
+#weight = [np.array([1,1,1,1,1,1,1,1,1]),np.array([1,1,1,1,1/200,1,1/10,1,.01]),np.array([1,1,1,1,1/200,1,1/10,1,1]),np.array([0,0,0,0,0,0,0,0,1]),np.array([1,1,1,1,1/200,1,1/10,0,1]),np.array([1,0,0,0,1/200,1,1/10,1,1]),np.array([1,0,0,0,1/200,1,1/10,1,1/100]),np.array([1,1/10,1/10,1/10,1/200,1,1/10,1,2]),np.array([1,1,1,1,1,1,1,1,5]),np.array([1,1,0,1,.01,.4,.3,.3,.1])]
+#weight = [np.array([1, 0, 0, 0, 0, 0, 1, 1, 0.1])]
+weight = [np.array([0, 0, 0, 0, 0, 0, 0, 0, 1])]
 for weights in weight:
 
     data = np.load('TestCase.npy')
@@ -168,7 +172,5 @@ for weights in weight:
     print(converted[:,0:7])
     print(mfcc_diffs)
     converted = np.dot(converted[:,0:7],np.diag(weights[0:7]))
-    cs, mus, cluster_mfcc_dists, cluster_tag_dists = k_means2(converted[0:num_songs_to_cluster], 3, 20, mfcc_diffs, tag_diffs)
+    cs, mus, cluster_mfcc_dists, cluster_tag_dists = k_means(converted[0:num_songs_to_cluster], 3, 20, mfcc_diffs, tag_diffs)
     print(cs)
-
-